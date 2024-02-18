c53b47979adf33385899d35fd230d0e1fc1b3300
Josh Mendoza <jacinto.josh.mendoza@gmail.com>
2024-02-18 12:24:30 -0600

    Data persistence with API requests

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from 'axios';
 
 function App() {
     const [books, setBooks] = useState([]);
 
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    };
+
     // called by BookShow
     // App.js <- BookList.js <- BookShow.js <- BookEdit.js
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         })
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
         const updatedBooks = [
             ...books,
-            { id: Math.round(Math.random() * 9999), title: title }
+            response.data
         ]
         setBooks(updatedBooks);
     };
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle }
+                return { ...book, ...response.data }
             }
             return book;
         })
         setBooks(updatedBooks);
     }
+
+    useEffect(() => { // similar to ngOnInit() as it's coded right now;
+        fetchBooks();
+    }, []); // this argument controls whether this will be called on follow-up renders
+    // to re-render everytime, don't add anything
+    // Add state variable to make it run whenever state variable is changed
 
     return (
         <div className="app">
